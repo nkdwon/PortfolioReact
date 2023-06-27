8513f1601201153f81df405414d1af75d21e4fc7
Felipe <felipebarrosratton.almeida@gmail.com>
2023-06-26 22:46:47 -0300

    update header

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import { useState } from 'react'
 import './App.css'
 
 function App() {
---
+++ portfolio/src/components/layout/Header.jsx
@@ -0,0 +1,16 @@
+import './header.css'
+
+function Header() {
+  return (
+    <div id='header'>
+      <ul id='lista'>
+        <li><a href="">Home</a> </li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Projetos</a></li>
+        <li><a href="">Contato</a></li>
+      </ul>
+    </div>
+  )
+}
+  
+export default Header
