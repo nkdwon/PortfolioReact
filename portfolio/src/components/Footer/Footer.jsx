bfaeff013804eaa65a80e45f8413632f9e749a67
Felipe <felipebarrosratton.almeida@gmail.com>
2023-07-02 13:09:28 -0300

    create page home and updates

---
+++ portfolio/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito por Felipe Barros © 2023
+        </footer>
+    )
+}
+
+export default Footer
