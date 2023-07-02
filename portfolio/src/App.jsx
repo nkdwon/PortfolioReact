248f4c44385364fa59fd23e2f3b84964f083493c
Felipe <felipebarrosratton.almeida@gmail.com>
2023-07-02 15:28:55 -0300

    update pages/components and create routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,28 +1,10 @@
 import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import AppRoutes from './routes'
 
 function App() {
+
   return (
-    <>
-    <Header/>
-    <section className='container'>
-      <div className='apresentacao'>
-        <p>
-          Olá, sou <br/>
-          <span>Felipe Barros</span> <br/>
-          Dev Full Stack
-        </p>
-        <button className='btn btn-red'>
-          Saiba mais sobre mim
-        </button>
-     </div>
-     <figure>
-        <img className='img-home' src="/src/assets/icon.svg" alt="Imagem de Home" />
-     </figure>
-    </section>
-    <Footer />
-    </>
+    <AppRoutes />
   )
 }
 
