7c145bf5486d22a5234e4de4f378dbe50df7bfe0
Felipe <felipebarrosratton.almeida@gmail.com>
2023-07-03 20:56:22 -0300

    create layout page and update routes

--- portfolio/src/routes.jsx
+++ portfolio/src/routes.jsx
@@ -4,26 +4,21 @@ import Sobre from './pages/Sobre/Sobre'
 import Projetos from './pages/Projetos/Projetos'
 import Contato from './pages/Contato/Contato'
 import Page404 from './pages/Page404/Page404'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Container from './components/Container/Container'
-
+import Layout from './pages/Layout/Layout'
 
 function AppRoutes() {
     return (
-        <BrowserRouter>
-          <Header/>
-            <Container>
-                <Routes>
-                    <Route path="/" element={<Home />} ></Route>
-                    <Route path="/sobre" element={<Sobre />} ></Route>
-                    <Route path="/projetos" element={<Projetos />} ></Route>
-                    <Route path="/contatos" element={<Contato />} ></Route>
-                    <Route path="*" element={<Page404 />} ></Route>
-                 </Routes>
-            </Container>
-          <Footer/>
-        </BrowserRouter>
+      <BrowserRouter>
+      <Routes>
+          <Route path='/' element={ <Layout /> }>
+              <Route index element={ <Home /> }></Route>
+              <Route path="/sobre" element={ <Sobre /> }></Route>
+              <Route path="/projetos" element={ <Projetos /> }></Route>
+              <Route path="/contatos" element={ <Contato /> }></Route>
+              <Route path="*" element={ <Page404 /> }></Route>
+          </Route>
+      </Routes>
+  </BrowserRouter>
     )
 }
 
