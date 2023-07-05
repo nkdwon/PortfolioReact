a11faf8d7a0d035c0301aaa095f26ac54605897c
Felipe <felipebarrosratton.almeida@gmail.com>
2023-07-05 15:11:39 -0300

    desafio

---
+++ portfolio/src/components/CardLanguages/CardLanguages.jsx
@@ -0,0 +1,27 @@
+import styles from './CardLanguages.module.css'
+
+function CardLanguages() {
+
+ /*  const [ repositories, setRepositories ] = useState([])
+
+  const languages = Object.keys(data)
+
+  useEffect(() => {
+      const buscarRepositorios = async () => {
+          const response = await fetch('https://api.github.com/users/nkdwon/repos')
+          const data = await response.json()
+          setRepositories(data)
+      }
+      buscarRepositorios()
+  }, [])
+
+  `https://api.github.com/repos/nkdwon/`{$nome_do_repositorio}`/languages` */
+
+  return (
+    <div className={styles.card_icones}>
+      
+    </div>
+  )
+}
+
+export default CardLanguages
